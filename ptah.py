6bcb1eedf138a92b09bc1294f7176fe4554922be
GT.Barnes <magna@magna.com>
2023-05-24 20:58:41 +0100

    Tidying up code

--- ptah.py
+++ ptah.py
@@ -2,8 +2,6 @@
 # Notes
 # ask for amount
 # clients, teamservers, rediretors, DCs, workstations, users
-# make it so you can pick exactly what you want e.g.:
-# ./create.py -c 1 -dc 1 -wk 5 -u 50
 # Assume breach option -> give beacon -> gives random shell as a user on a Workstation
 
 # FIX
@@ -96,6 +94,16 @@ def splash():
         print(colored(i, random.choice(COLOURS)))
     print(colored(DIVIDE, border_colour))
 
+def print_menu(menu):
+    for key in menu:
+        tab = key + ": "
+
+        print(colored(tab, 'white'), end = "") # e.g. 1:
+        print(colored(menu[key], 'blue'))
+
+    choice = get_input(menu)
+    return choice
+
 def get_input(possibles):
     cursor = "Choice -> "
     choice = ""
@@ -105,45 +113,7 @@ def get_input(possibles):
         choice = input()
     return choice
 
-def validate_ip_address(ip_string):
-   try:
-        ip_object = ipaddress.ip_address(ip_string)
-        return True
-   except ValueError:
-        if ip_string == "": return False
-        else:
-            print(colored("Not a valid IP!", "red"))
-            return False
-
-def validate_url(url):
-    validation = validators.url(url)
-    if url == "jim": return False
-    
-    if validation:
-        return True
-    else:
-        print(colored("Not a valid URL!", "red"))
-        return False
-
-def get_exchange():
-    try:
-        with open(EX2016, 'r') as f:
-            print(colored("Exchange 2016 ISO found, SMASHING!", 'blue'))
-
-    except IOError:
-        print(colored("Exchange 2016 ISO not found, BOGUS!", 'red'))
-        print(colored("Wish to download Exchange 2016 ISO:", 'green'))
-        ans = print_menu(YESNO)
-        if(ans == "1"):
-            print(colored("Downloading ~5.6GiB Please Wait!... ", 'blue'))
-            urllib.request.urlretrieve('https://download.microsoft.com/download/2/5/8/258D30CF-CA4C-433A-A618-FB7E6BCC4EEE/ExchangeServer2016-x64-cu12.iso', EX2016)
-            print(colored("Download Complete :)", 'blue'))
-        if(ans == "2"):
-            print(colored("Come back when you've got the ISO buddy!", 'red'))
-            quit()
-
-
-def get_ips(amount):
+def get_ip_address(amount):
     cursor = "IP -> "
     choice = "jim"
     ip = ""
@@ -161,6 +131,16 @@ def get_ips(amount):
 
     return concat_ips[:-1]
 
+def validate_ip_address(ip_string):
+   try:
+        ip_object = ipaddress.ip_address(ip_string)
+        return True
+   except ValueError:
+        if ip_string == "": return False
+        else:
+            print(colored("Not a valid IP!", "red"))
+            return False
+
 def get_url():
     cursor = "URL -> "
     url = "jim"
@@ -170,99 +150,130 @@ def get_url():
         url = input()
     return url
 
-def clear_inventory():
-    # Open file for reading
-    with open(f'{INVENTORY}','r') as f:
-        output = yaml.safe_load(f)
-    # Clear all IPs
-    for host in LINUX:
-        output["linux"]["children"][host]["hosts"] = None
-    for host in WINDOWS:
-        output["windows"]["children"][host]["hosts"] = None
+def validate_url(url):
+    validation = validators.url(url)
+    if url == "jim": return False
     
-    # Save new inventory
-    with open(f'{INVENTORY}', 'w') as f:
-        yaml.safe_dump(output,f , sort_keys=False)
-
-def edit_inventory():
-    global inven_vars
-
-    windows_found = False
-    clear_inventory()
-    # Open file for reading
-    with open(f'{INVENTORY}','r') as f:
-        output = yaml.safe_load(f)
+    if validation:
+        return True
+    else:
+        print(colored("Not a valid URL!", "red"))
+        return False
     
-    for host in inventory:
-        # Check for empty amount
-        if host[1] == "": continue
+
+class Deploy():
+    def deploy(self):
+        for i in inventory:
+            text = "Amount of " + i[0] + "'s: "
+
+            print(colored(text, 'green'))
+            choice = get_input(DIGITS)
+
+            i[1] = choice
+            i[2] = get_ip_address(i[1])
+
+            if(i[0] == "exchange" and i[2] != ""):
+                self.get_exchange()
+
+        self.edit_inventory()
+
+    def edit_inventory(self):
+        global inven_vars
+
+        windows_found = False
+        self.clear_inventory()
+
+        # Open file for reading
+        with open(f'{INVENTORY}','r') as f:
+            output = yaml.safe_load(f)
         
-        # Get variables from list
-        # ['attacker', '2', '2.2.2.2~3.3.3.3.3']
-        host_type = host[0]
-        amount = int(host[1])
-        ips = host[2].split("~")
+        for host in inventory:
+            # Check for empty amount
+            if host[1] == "": continue
+            
+            # Get variables from list
+            # ['attacker', '2', '2.2.2.2~3.3.3.3.3']
+            host_type = host[0]
+            amount = int(host[1])
+            ips = host[2].split("~")
 
-        # I hope nobody ever reads this code....
-        if(host_type in LINUX and host[1] != "0"):
-            output["linux"]["children"][host_type]["hosts"] = {"temp":"temp"}
-        if(host_type in WINDOWS and host[1] != "0"):
-            output["windows"]["children"][host_type]["hosts"] = {"temp":"temp"}
+            # I hope nobody ever reads this code....
+            if(host_type in LINUX and host[1] != "0"):
+                output["linux"]["children"][host_type]["hosts"] = {"temp":"temp"}
+            if(host_type in WINDOWS and host[1] != "0"):
+                output["windows"]["children"][host_type]["hosts"] = {"temp":"temp"}
 
-        # For amount of addresses add to dict
-        for a in range(amount):
-            hostname = host_type + "{:02d}".format(a + 1)
+            # For amount of addresses add to dict
+            for a in range(amount):
+                hostname = host_type + "{:02d}".format(a + 1)
 
-            if(host_type in LINUX):
-                output["linux"]["children"][host_type]["hosts"][hostname] = {'ansible_host': ips[a]}
-                output["linux"]["children"][host_type]["hosts"].pop("temp", None)
-            if(host_type in WINDOWS):
-                windows_found = True
-                output["windows"]["children"][host_type]["hosts"][hostname] = {'ansible_host': ips[a]}
-                output["windows"]["children"][host_type]["hosts"].pop("temp", None)
+                if(host_type in LINUX):
+                    output["linux"]["children"][host_type]["hosts"][hostname] = {'ansible_host': ips[a]}
+                    output["linux"]["children"][host_type]["hosts"].pop("temp", None)
+                if(host_type in WINDOWS):
+                    windows_found = True
+                    output["windows"]["children"][host_type]["hosts"][hostname] = {'ansible_host': ips[a]}
+                    output["windows"]["children"][host_type]["hosts"].pop("temp", None)
 
-  
-    # Get gatway and subnet mask for Windows hosts
-    if(windows_found):
-        # Gateway
-        print(colored("Windows Gateway IP: ", 'green'))
-        choice = get_ips(1)
-        temp = {"windows_gateway": choice}
-        inven_vars.update(temp)
+    
+        # Get gatway and subnet mask for Windows hosts
+        if(windows_found):
+            # Gateway
+            print(colored("Windows Gateway IP: ", 'green'))
+            choice = get_ip_address(1)
+            temp = {"windows_gateway": choice}
+            inven_vars.update(temp)
 
-        # Subnet Mask
-        print(colored("Windows Subnet Mask: ", 'green'))
-        choice = get_ips(1)
-        temp = {"windows_mask": choice}
-        inven_vars.update(temp)
+            # Subnet Mask
+            print(colored("Windows Subnet Mask: ", 'green'))
+            choice = get_ip_address(1)
+            temp = {"windows_mask": choice}
+            inven_vars.update(temp)
 
-        # Add to output
-        output["windows"]["vars"] = inven_vars
+            # Add to output
+            output["windows"]["vars"] = inven_vars
+            
+        # Save new inventory
+        try:
+            with open(f'{INVENTORY}', 'w') as f:
+                yaml.safe_dump(output,f , sort_keys=False)
+            print(colored("Inventory Saved!", 'blue'))
+            print(colored("Please Run: ", 'blue'), end = '')
+            print(colored("ansible-playbook -i inventory.yml deploy.yml", 'green'))
+        except:
+            print(colored("Inventory failed to save... oh noe", 'red'))
+
+    def clear_inventory(self):
+        # Open file for reading
+        with open(f'{INVENTORY}','r') as f:
+            output = yaml.safe_load(f)
+        # Clear all IPs
+        for host in LINUX:
+            output["linux"]["children"][host]["hosts"] = None
+        for host in WINDOWS:
+            output["windows"]["children"][host]["hosts"] = None
         
-    # Save new inventory
-    try:
+        # Save new inventory
         with open(f'{INVENTORY}', 'w') as f:
             yaml.safe_dump(output,f , sort_keys=False)
-        print(colored("Inventory Saved!", 'blue'))
-        print(colored("Please Run: ", 'blue'), end = '')
-        print(colored("ansible-playbook -i inventory.yml deploy.yml", 'green'))
-    except:
-        print(colored("Inventory failed to save... oh noe", 'red'))
 
-def deploy():
-    for i in inventory:
-        text = "Amount of " + i[0] + "'s: "
+    #### Purely for downloading the exchange ISO ####
+    def get_exchange(self):
+        try:
+            with open(EX2016, 'r') as f:
+                print(colored("Exchange 2016 ISO found, SMASHING!", 'blue'))
 
-        print(colored(text, 'green'))
-        choice = get_input(DIGITS)
-
-        i[1] = choice
-        i[2] = get_ips(i[1])
-
-        if(i[0] == "exchange" and i[2] != ""):
-            get_exchange()
-
-    edit_inventory()
+        except IOError:
+            print(colored("Exchange 2016 ISO not found, BOGUS!", 'red'))
+            print(colored("Wish to download Exchange 2016 ISO:", 'green'))
+            ans = print_menu(YESNO)
+            if(ans == "1"):
+                print(colored("Downloading ~5.6GiB Please Wait!... ", 'blue'))
+                urllib.request.urlretrieve('https://download.microsoft.com/download/2/5/8/258D30CF-CA4C-433A-A618-FB7E6BCC4EEE/ExchangeServer2016-x64-cu12.iso', EX2016)
+                print(colored("Download Complete :)", 'blue'))
+            if(ans == "2"):
+                print(colored("Come back when you've got the ISO buddy!", 'red'))
+                quit()
 
 def breach():
     # Load breach facts
@@ -320,18 +331,11 @@ def breach():
         print(colored("ok bye!", 'blue'))
         return 0
 
-def print_menu(menu):
-    for key in menu:
-        tab = key + ": "
 
-        print(colored(tab, 'white'), end = "") # e.g. 1:
-        print(colored(menu[key], 'blue'))
-
-    choice = get_input(menu)
-    return choice
 
 if __name__ == "__main__":
     splash()
     choice = print_menu(MAIN)
-    if(choice == "1"): deploy()
+    new_deploy = Deploy()
+    if(choice == "1"): new_deploy.deploy()
     if(choice == "2"): breach()
